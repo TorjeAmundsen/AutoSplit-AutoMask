02799360e87048296c80673951687e56f55f7fb5
Torje Amundsen <torjefrafen@hotmail.no>
2025-03-05 03:08:59 +0100

    axaml part done, start of actual functionality

--- AutoMask/Views/MainWindow.axaml.cs
+++ AutoMask/Views/MainWindow.axaml.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using AutoMask.ViewModels;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
 
 namespace AutoMask.Views;
 
@@ -11,29 +14,53 @@ public partial class MainWindow : Window
     private readonly string AutoMaskSemVer = "0.2.0";
     private readonly string? VersionSuffix = "alpha";
     
-    public List<string> PresetComboBoxItems { get; set; }
-    public List<string> SplitsComboBoxItems { get; set; }
-    public List<string> InputImageComboBoxItems { get; set; }
+    private List<string> LoadedInputImagePaths { get; set; } = new();
+    private List<string> LoadedAlphaImagePaths { get; set; } = new();
+
+    private string? selectedInputImagePath => LoadedInputImagePaths.Count > 0 && ComboBoxSelectInputImage.SelectedIndex < LoadedInputImagePaths.Count ? LoadedInputImagePaths[ComboBoxSelectInputImage.SelectedIndex] : null;
+    private string? selectedAlphaImagePath => LoadedAlphaImagePaths.Count > 0 ? LoadedAlphaImagePaths[ComboBoxSelectSplit.SelectedIndex] : null;
+
     public MainWindow()
     {
         InitializeComponent();
 
-        DataContext = this;
+        ComboBoxSelectInputImage.Items.Add("No input images loaded");
 
         Title = "AutoMask v" + AutoMaskSemVer + (string.IsNullOrEmpty(VersionSuffix) ? string.Empty : "-" + VersionSuffix);
     }
-    
-    public string TestString { get; set; } = "AutoMask";
 
-    private void BtnLoadInputImage_OnClick(object? sender, RoutedEventArgs e)
+    private async void BtnLoadInputImage_OnClick(object? sender, RoutedEventArgs e)
     {
-        BtnLoadInputImage.Content = "Foo bar";
+        var files = await StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "Select input image(s) to load",
+            AllowMultiple = true,
+        });
+
+        if (files.Count >= 1)
+        {
+            ComboBoxSelectInputImage.SelectedIndex = 0;
+            ComboBoxSelectInputImage.Items.Clear();
+            LoadedInputImagePaths.Clear();
+            
+            foreach (var file in files)
+            {
+                ComboBoxSelectInputImage.Items.Add(file.Name);
+                LoadedInputImagePaths.Add(file.Path.LocalPath);
+            }
+            
+            ComboBoxSelectInputImage.IsEnabled = true;
+
+            if (ComboBoxSelectInputImage.Items.Count > 0)
+            {
+                ComboBoxSelectInputImage.SelectedIndex = 0;
+            }
+        }
     }
 
     private void BtnOpenOutputFolder_OnClick(object? sender, RoutedEventArgs e)
     {
-        BtnLoadInputImage.Content = "https://github.com/AutoMask/AutoMask";
-        Console.WriteLine(TestString);
+        throw new NotImplementedException();
     }
 
     private void BtnOpenPresetsFolder_OnClick(object? sender, RoutedEventArgs e)
@@ -43,7 +70,7 @@ public partial class MainWindow : Window
 
     private void ComboBoxSelectInputImage_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        Console.WriteLine(selectedInputImagePath);
     }
 
     private void BtnNextAlphaImage_OnClick(object? sender, RoutedEventArgs e)
@@ -63,12 +90,22 @@ public partial class MainWindow : Window
 
     private void BtnNextInputImage_OnClick_OnClick(object? sender, RoutedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        if (ComboBoxSelectInputImage.SelectedIndex == ComboBoxSelectInputImage.Items.Count - 1)
+        {
+            return;
+        }
+        
+        ComboBoxSelectInputImage.SelectedIndex += 1;
     }
 
     private void BtnPrevInputImage_OnClick_OnClick(object? sender, RoutedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        if (ComboBoxSelectInputImage.SelectedIndex == 0)
+        {
+            return;
+        }
+        
+        ComboBoxSelectInputImage.SelectedIndex -= 1;
     }
 
     private void ComboBoxSelectSplit_OnSelectionChanged_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
