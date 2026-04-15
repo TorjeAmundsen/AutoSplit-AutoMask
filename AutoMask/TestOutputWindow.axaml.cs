359c8897259baa434eeade3e1ae5a474e95419fe
Torje Amundsen <torjefrafen@hotmail.no>
2026-04-15 18:49:58 +0200

    clean up tester window

--- AutoMask/TestOutputWindow.axaml.cs
+++ AutoMask/TestOutputWindow.axaml.cs
@@ -444,6 +444,22 @@ public partial class TestOutputWindow : Window
         _hasUserChanges = true;
     }
 
+    private void BtnCropDecrement_Click(object? sender, RoutedEventArgs e)
+    {
+        if (sender is Button { Tag: string name } && this.FindControl<NumericUpDown>(name) is { } nud)
+        {
+            nud.Value = (nud.Value ?? 0) - nud.Increment;
+        }
+    }
+
+    private void BtnCropIncrement_Click(object? sender, RoutedEventArgs e)
+    {
+        if (sender is Button { Tag: string name } && this.FindControl<NumericUpDown>(name) is { } nud)
+        {
+            nud.Value = (nud.Value ?? 0) + nud.Increment;
+        }
+    }
+
     private void BtnResetCrop_Click(object? sender, RoutedEventArgs e)
     {
         ResetCropToFull();
